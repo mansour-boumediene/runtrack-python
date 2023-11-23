353737f32d09ce245cb6b2361afa7c00b132c3a6
mansour-boumediene <146710492+mansour-boumediene@users.noreply.github.com>
2023-11-23 10:11:18 +0100

    Update Job07.py

--- Jour03/Job07/Job07.py
+++ Jour03/Job07/Job07.py
@@ -1,23 +1,14 @@
-def moyenne():
-    note1 = input("saisisez la premiéres note")
-    note2 = input("saisisez la seconde note")
-    note3 = input("saisisez la troisieme note")
-
-    result = float(note1) + float(note2) + float(note3)
-    division = result / 3
-
-    moyenne_etudiant = division
-
-    if moyenne_etudiant >= 15 and moyenne_etudiant <= 20:
-        print("Trés bon élève")
-    elif moyenne_etudiant <=14 and moyenne_etudiant >=11:
-        print("Bon élève")
-    elif moyenne_etudiant >=8 and moyenne_etudiant <=10:
-        print("Élève moyen")
-    elif moyenne_etudiant >=0 and moyenne_etudiant <=7:
-        print("Élève devant faires des efforts")
-    return moyenne_etudiant
-
-
-print(moyenne())
-
+def message(langage):
+    if langage == "Javascript":
+        return "Tu es un développeur web"
+    if langage == "python":
+        return "tu es un développeur IA"
+    if langage == "java":
+        return "tu es un développeur logiciel"
+    if langage == "reactjs":
+        return "tu es un développeur mobile"
+    else:
+        return "un jour, je serai le meilleur développeur"
+a=message("python15") 
+print(a)   
+    
