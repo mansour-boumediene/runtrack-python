e86351291e54136e8ae6a2cd03b12f3b51992d78
mansour-boumediene <146710492+mansour-boumediene@users.noreply.github.com>
2023-11-26 13:50:04 +0100

    Update Job12.py

--- Jour04/job12/Job12.py
+++ Jour04/job12/Job12.py
@@ -1,7 +1,20 @@
-my_list = [8, 2]
+def my_liste(liste):
+    taille = 0
+    for _ in liste:
+        taille += 1
+    return taille
 
-for i in my_list:
+def tri_croissant(liste):
+    n = my_liste(liste)
+    for i in range(n - 1):
+        indice_min = i
+        for j in range(i + 1, n):
+            if liste[j] < liste[indice_min]:
+                indice_min = j
+        liste[i], liste[indice_min] = liste[indice_min], liste[i]
 
-    if ([8]) > ([2]):
-        del my_list[0]
-        print(my_list)
+    return liste
+
+ma_liste = [124, 95, 2, 12, 191]
+resultat = tri_croissant(ma_liste)
+print("Liste triée dans l'ordre croissant :", resultat)
